35d522f4b71ec11eb6fe822da2835bd0bea0cd75
Niles Bernabe <nmbernabe@crimson.ua.edu>
2019-03-28 08:05:53 -0500

    variable board size

--- .vscode/connect4.c
+++ .vscode/connect4.c
@@ -2,37 +2,43 @@
 #include <string.h>
 #include <stdlib.h>
 
-void printBoard(char *board);
-int takeTurn(char *board, int player, const char*);
-int checkWin(char *board);
+void printBoard(char *board, int , int);
+int takeTurn(char *board, int player, int, int, const char*);
+int checkWin(char *board, int, int);
 int checkFour(char *board, int, int, int, int);
-int horizontalCheck(char *board);
-int verticalCheck(char *board);
-int diagonalCheck(char *board);
+int horizontalCheck(char *board, int, int);
+int verticalCheck(char *board, int, int);
+int diagonalCheck(char *board, int, int);
 const int BOARD_ROWS = 6;
 const int BOARD_COLS = 7;
 
 int main(int argc, char *argv[]){
-
+    int columns = 0;
+    int rows = 0;
    printf("How many columns on the board (standard is 7):");
+   scanf("%d", &columns);
+   printf("How many rows on the board (standard is 6):");
+   scanf("%d", &rows);
+
+   printf("%d %d", columns, rows);
 
    const char *PIECES = "XO";
-   char board[BOARD_ROWS * BOARD_COLS];
-   memset(board, ' ', BOARD_ROWS * BOARD_COLS);
+   char board[rows * columns];
+   memset(board, ' ', rows * columns);
 
    int turn, done = 0;
 
-   for(turn = 0; turn < BOARD_ROWS * BOARD_COLS && !done; turn++){
-      printBoard(board);   
-      while(!takeTurn(board, turn % 2, PIECES)){
-         printBoard(board);   
+   for(turn = 0; turn < rows * columns && !done; turn++){
+      printBoard(board, rows, columns);   
+      while(!takeTurn(board, turn % 2, rows, columns, PIECES)){
+         printBoard(board, rows, columns);   
          printf("**Column full!**\n");
       }
-      done = checkWin(board);
+      done = checkWin(board, rows, columns);
    } 
-   printBoard(board);
+   printBoard(board, rows, columns);
 
-   if(turn == BOARD_ROWS * BOARD_COLS && !done){
+   if(turn == rows * columns && !done){
       printf("It's a tie!");
    } else {
       turn--;
@@ -42,26 +48,34 @@ int main(int argc, char *argv[]){
    return 0;
 
 }
-void printBoard(char *board){
+void printBoard(char *board, int r, int c){
    system("clear");
    puts("\n    ****Connect Four****\n");
-   for(int row = 0; row < BOARD_ROWS; row++){
-      for(int col = 0; col < BOARD_COLS; col++){
-         printf("| %c ",  board[BOARD_COLS * row + col]);
+   for(int row = 0; row < r; row++){
+      for(int col = 0; col < c; col++){
+         printf("| %c ",  board[c * row + col]);
       }
       puts("|");
-      puts("-----------------------------");
+      printf("-");
+      for (int i = 0; i < c; i++) {
+          printf("----");
+      }
+      printf("\n");
 
    }
-   printf("  1   2   3   4   5   6   7\n");
+   printf("  1");
+   for (int i = 1; i < c; i++) {
+       printf("   %d", i+1);
+   }
+   printf("\n");
 
 }
-int takeTurn(char *board, int player, const char *PIECES){
+int takeTurn(char *board, int player, int r, int c, const char *PIECES){
    int row, col = 0;
    printf("Player %d (%c):\nEnter number coordinate: ", player + 1, PIECES[player]);
 
    while(1){ 
-      if(1 != scanf("%d", &col) || col < 1 || col > 7 ){
+      if(1 != scanf("%d", &col) || col < 1 || col > c ){
          while(getchar() != '\n');
          printf("Number out of bounds! Try again.");
       } else { 
@@ -70,30 +84,30 @@ int takeTurn(char *board, int player, const char *PIECES){
    }
    col--;
 
-   for(row = BOARD_ROWS - 1; row >= 0; row--){
-      if(board[BOARD_COLS * row + col] == ' '){
-         board[BOARD_COLS * row + col] = PIECES[player];
+   for(row = r - 1; row >= 0; row--){
+      if(board[c * row + col] == ' '){
+         board[c * row + col] = PIECES[player];
          return 1;
       }
    }
    return 0;
 
 }
-int checkWin(char *board){
-    return (horizontalCheck(board) || verticalCheck(board) || diagonalCheck(board));
+int checkWin(char *board, int r, int c){
+    return (horizontalCheck(board, r, c) || verticalCheck(board, r ,c) || diagonalCheck(board, r, c));
 
 }
 int checkFour(char *board, int a, int b, int c, int d){
     return (board[a] == board[b] && board[b] == board[c] && board[c] == board[d] && board[a] != ' ');
 
 }
-int horizontalCheck(char *board){
+int horizontalCheck(char *board, int r, int c){
     int row, col, idx;
     const int WIDTH = 1;
 
-    for(row = 0; row < BOARD_ROWS; row++){
-       for(col = 0; col < BOARD_COLS - 3; col++){
-          idx = BOARD_COLS * row + col;
+    for(row = 0; row < r; row++){
+       for(col = 0; col < c - 3; col++){
+          idx = c * row + col;
           if(checkFour(board, idx, idx + WIDTH, idx + WIDTH * 2, idx + WIDTH * 3)){
              return 1;
           }
@@ -102,13 +116,13 @@ int horizontalCheck(char *board){
     return 0;
 
 }
-int verticalCheck(char *board){
+int verticalCheck(char *board, int r, int c){
     int row, col, idx;
-    const int HEIGHT = 7;
+    const int HEIGHT = c;
 
-    for(row = 0; row < BOARD_ROWS - 3; row++){
-       for(col = 0; col < BOARD_COLS; col++){
-          idx = BOARD_COLS * row + col;
+    for(row = 0; row < r - 3; row++){
+       for(col = 0; col < c; col++){
+          idx = c * row + col;
           if(checkFour(board, idx, idx + HEIGHT, idx + HEIGHT * 2, idx + HEIGHT * 3)){
               return 1;
           }
@@ -117,13 +131,13 @@ int verticalCheck(char *board){
     return 0;
 
 }
-int diagonalCheck(char *board){
+int diagonalCheck(char *board, int r, int c){
    int row, col, idx, count = 0;
-   const int DIAG_RGT = 6, DIAG_LFT = 8;
+   const int DIAG_RGT = (c - 1), DIAG_LFT = (c + 1);
 
-   for(row = 0; row < BOARD_ROWS - 3; row++){
-      for(col = 0; col < BOARD_COLS; col++){
-         idx = BOARD_COLS * row + col;
+   for(row = 0; row < r - 3; row++){
+      for(col = 0; col < c; col++){
+         idx = c * row + col;
          if(count <= 3 && checkFour(board, idx, idx + DIAG_LFT, idx + DIAG_LFT * 2, idx + DIAG_LFT * 3) || count >= 3 && checkFour(board, idx, idx + DIAG_RGT, idx + DIAG_RGT * 2, idx + DIAG_RGT * 3)){
             return 1;
          }
